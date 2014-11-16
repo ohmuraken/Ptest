b1ed2253ecaeb70ae81b878f17038cf17c1eb296
ohmuraken <kenkenmurama2@gmail.com>
2014-11-17 00:53:15 +0900

    Improve Error of name and get_page

--- test9.py
+++ test9.py
@@ -1,16 +1,25 @@
 #coding: UTF-8
+def get_page(url):
+	try:
+	    import urllib
+	    return urllib.urlopen(url).read()
+
+	except:
+	    return "error"
+
 def get_next_target(page):
 	start_link = page.find('<a href=')
 	if start_link==-1:
 		return None, 0;
 	start_quote = page.find('"',start_link)
 	end_quote = page.find('"',start_quote+1)
-	url = pages[start_quote+1:end_quote]
+	url = page[start_quote+1:end_quote]
 	return url, end_quote 
 
 
 
-def get_all_links(pages):
+
+def get_all_links(page):
 	while True:
 		url ,endpos = get_next_target(page)
 		if url:
@@ -19,9 +28,10 @@ def get_all_links(pages):
 		else:
 			break
 
-page = '<a href="result1">link1</a><a href="result2">link2</a><a href="result3">link3</a>'
+#page = '<a href="result1">link1</a><a href="result2">link2</a><a href="result3">link3</a>'
+print get_all_links(get_page('https://camph.net'))
 
-get_all_links(page)
+#get_all_links(page)
 
 
 
